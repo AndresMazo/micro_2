34fabcf008383ecbf226288c70d7cb8ea4130f52
AndresMazo <112454128+AndresMazo@users.noreply.github.com>
2022-10-27 10:38:52 -0500

    pro

--- main.ts
+++ main.ts
@@ -1,14 +1,25 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Alexander Pro")
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showString("Alexander Pro")
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(3)
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+    basic.pause(100)
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.B, function () {
+    radio.sendString("" + (input.temperature()))
     basic.showString("" + (input.temperature()))
+    radio.sendString("" + (input.lightLevel()))
     basic.showString("" + (input.lightLevel()))
 })
 input.onGesture(Gesture.Shake, function () {
+    radio.sendString("")
     for (let index = 0; index < 4; index++) {
         basic.showIcon(IconNames.Happy)
         basic.pause(200)
